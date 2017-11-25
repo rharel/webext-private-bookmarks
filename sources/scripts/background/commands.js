17e57ca2c1fcea467484b2de54f9c7f948372bf4
Raoul Harel <raoul.harel@gmail.com>
2017-11-25 21:35:02 +0100

    Use the bookmarks manager module instead of its subcomponent

--- sources/scripts/background/commands.js
+++ sources/scripts/background/commands.js
@@ -1,23 +1,23 @@
 (function()
 {
     /// Set in define().
-    let core, notification;
+    let bookmarks, notification;
 
     /// Lock in response to user command.
     browser.commands.onCommand.addListener(async command =>
     {
-        if (command === "lock" && core.is_unlocked())
+        if (command === "lock" && bookmarks.is_unlocked())
         {
-            await core.lock();
+            await bookmarks.lock();
             notification.locked();
         }
     });
 
-    require(["scripts/background/bookmarks_manager/core",
+    require(["scripts/background/bookmarks_manager",
              "scripts/utilities/notification"],
-            (core_module, notification_module) =>
+            (bookmarks_module, notification_module) =>
             {
-                core = core_module;
+                bookmarks = bookmarks_module;
                 notification = notification_module;
             });
 })();
