7547d5ea8574b9d46684f3a32c4741fe29232eb3
Raoul Harel <raoul.harel@gmail.com>
2017-11-27 17:12:12 +0100

    Simplify version module

--- sources/scripts/meta/version.js
+++ sources/scripts/meta/version.js
@@ -1,51 +1,27 @@
 (function()
 {
-    /// Set in define().
-    let clamp;
-
-    /// Minimum and maximum component values.
-    const MINIMUM_COMPONENT = 0,
-          MAXIMUM_COMPONENT = 65535;
-
-    /// Creates a new version object with the given major, minor, and release components.
-    /// All components must be integers in the range [0, 65535].
-    function create(major, minor, release)
+    const [major, minor, release] = (
+        browser.runtime.getManifest().version
+            .split(".")
+            .map(component_string => parseInt(component_string))
+    );
+    const CURRENT =  /// The current version identifier.
     {
-        return  {
-                    major:   clamp(major,   MINIMUM_COMPONENT, MAXIMUM_COMPONENT),
-                    minor:   clamp(minor,   MINIMUM_COMPONENT, MAXIMUM_COMPONENT),
-                    release: clamp(release, MINIMUM_COMPONENT, MAXIMUM_COMPONENT)
-                };
-    }
-
-    const CURRENT = (function()
-    {
-        const [major, minor, release] = (
-            browser.runtime.getManifest().version
-                .split(".")
-                .map(component_string => parseInt(component_string))
-        );
-        return create(major, minor, release);
-    })();
-
-    /// Release notes are hosted in this directory.
+        major:   major,
+        minor:   minor,
+        release: release
+    };
+    /// Release notes are hosted here.
     const RELEASE_NOTES_URL = (
         "https://rharel.github.io/webext-private-bookmarks/release-notes/" +
-        `${CURRENT.major}-${CURRENT.minor}-${CURRENT.release}.html`
+        `${major}-${minor}-${release}.html`
     );
-
-    define(["scripts/utilities/math"],
-           math_module =>
-           {
-                clamp = math_module.clamp;
-
-                return  {
-                            CURRENT: CURRENT,
-                            RELEASE_NOTES:
-                            {
-                                url: RELEASE_NOTES_URL,
-                                is_relevant_to_users: false
-                            }
-                        };
-           });
+    define({
+        CURRENT: CURRENT,
+        RELEASE_NOTES:
+        {
+            url: RELEASE_NOTES_URL,
+            is_relevant_to_users: false
+        }
+    });
 })();
