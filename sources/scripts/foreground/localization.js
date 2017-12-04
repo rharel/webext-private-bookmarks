8d8244a3803358bbf6b70660c8cfc92555e1cd18
Raoul Harel <raoul.harel@gmail.com>
2017-12-04 21:12:11 +0100

    Simplify DOM query string

--- sources/scripts/foreground/localization.js
+++ sources/scripts/foreground/localization.js
@@ -4,13 +4,13 @@
     const LOCALIZED_ATTRIBUTE = "data-localized-attribute";
     const LOCALIZED_ATTRIBUTE_VALUE = "data-localized-attribute-value";
 
-    document.querySelectorAll(`*[${LOCALIZED_TEXT}]`).forEach(element =>
+    document.querySelectorAll(`[${LOCALIZED_TEXT}]`).forEach(element =>
     {
         const message = browser.i18n.getMessage(element.getAttribute(LOCALIZED_TEXT));
 
         if (message !== "") { element.textContent = message; }
     });
-    document.querySelectorAll(`*[${LOCALIZED_ATTRIBUTE}]`).forEach(element =>
+    document.querySelectorAll(`[${LOCALIZED_ATTRIBUTE}]`).forEach(element =>
     {
         const message = browser.i18n.getMessage(element.getAttribute(LOCALIZED_ATTRIBUTE_VALUE));
 
