4fa0845353eb05a243da695c871eda0c3c1b1c67
Raoul Harel <raoul.harel@gmail.com>
2018-01-10 13:56:35 +0100

    Optimize event listeners in page action
    Previously event listeners were always hooked up, even while bookmarks were locked. Since the page action is irrelevant while bookmarks are locked we make sure that event listeners are only hooked up for the duration of an unlock.

--- sources/scripts/background/page_action.js
+++ sources/scripts/background/page_action.js
@@ -6,6 +6,18 @@
     /// True iff the extension's privacy context setting is set to private.
     let do_limit_to_private_context = false;
 
+    /// Identifiers of tabs known to show the page action.
+    let host_tab_ids = {};
+
+    /// Hides the page action in all tabs.
+    function hide_in_all_tabs()
+    {
+        for (const id_string in host_tab_ids)
+        {
+            browser.pageAction.hide(parseInt(id_string));
+        }
+        host_tab_ids = {};
+    }
     /// Shows/hides the page action in the specified tab.
     /// The action is visible when:
     ///     1. Its tab's context matches the extension's privacy context setting.
@@ -18,8 +30,13 @@
            (await bookmarks.contains_url(tab.url)))
         {
             browser.pageAction.hide(tab.id);
+            delete host_tab_ids[tab.id];
         }
-        else { browser.pageAction.show(tab.id); }
+        else
+        {
+            browser.pageAction.show(tab.id);
+            host_tab_ids[tab.id] = true;
+        }
     }
     /// Shows/hides the page action in active tabs.
     async function update_in_active_tabs()
@@ -40,35 +57,56 @@
     function handle_context_requirement_change(new_requirements)
     {
         do_limit_to_private_context = new_requirements.do_limit_to_private_context;
+        if (bookmarks.is_unlocked()) { update_in_active_tabs(); }
+    }
+
+    /// Shows/hides the page action in a recently activated tab.
+    async function on_tab_activated(info)
+    {
+        update_in_tab(await browser.tabs.get(info.tabId));
+    }
+    /// Shows/hides the page action in an recently loaded tab.
+    function on_tab_updated(id, change_info, tab)
+    {
+        if (tab.active && change_info.url) { update_in_tab(tab); }
+    }
+    /// Tracks closed tabs so that we don't attempt to show/hide the page action in those.
+    function on_tab_removed(id) { delete host_tab_ids[id]; }
+
+    /// Enables the page action.
+    function enable()
+    {
+        browser.bookmarks.onRemoved.addListener(update_in_active_tabs);
+        browser.tabs.onActivated.addListener(on_tab_activated);
+        browser.tabs.onUpdated.addListener(on_tab_updated);
+        browser.tabs.onRemoved.addListener(on_tab_removed);
+
         update_in_active_tabs();
+    }
+    /// Disables the page action.
+    function disable()
+    {
+        browser.bookmarks.onRemoved.removeListener(update_in_active_tabs);
+        browser.tabs.onActivated.removeListener(on_tab_activated);
+        browser.tabs.onUpdated.removeListener(on_tab_updated);
+        browser.tabs.onRemoved.removeListener(on_tab_removed);
+
+        hide_in_all_tabs();
     }
 
     /// Initializes this module.
     function initialize()
     {
-        events.local.add_listener(["lock", "unlock"], update_in_active_tabs);
+        events.local.add_listener("unlock", enable);
+        events.local.add_listener("lock",   disable);
         events.local.add_listener(
             "context-requirement-change",
             handle_context_requirement_change
         );
         storage.load(storage.Key.Configuration).then(handle_context_requirement_change);
 
-        /// When a bookmark is removed we may need to update the page action's visibility.
-        browser.bookmarks.onRemoved.addListener(update_in_active_tabs);
-
         /// Bookmarks the active tab's page when the action is activated.
         browser.pageAction.onClicked.addListener(bookmark_tab);
-
-        /// Shows/hides the page action in a recently activated tab.
-        browser.tabs.onActivated.addListener(async activation_info =>
-        {
-            update_in_tab(await browser.tabs.get(activation_info.tabId));
-        });
-        /// Shows/hides the page action in an recently loaded tab.
-        browser.tabs.onUpdated.addListener((id, change_info, tab) =>
-        {
-            if (tab.active && change_info.url) { update_in_tab(tab); }
-        });
     }
 
     define(["scripts/background/bookmarks_manager",
