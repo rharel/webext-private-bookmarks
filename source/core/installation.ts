2f145b0b44c9e57bc30a0c4b2e2498ee85c2912f
Raoul Harel <raoul.harel@gmail.com>
2022-06-12 17:21:51 +0200

    only show release notes of user indicated so

--- source/core/installation.ts
+++ source/core/installation.ts
@@ -1,5 +1,6 @@
 import { browser } from "webextension-polyfill-ts";
 
+import { options } from "./options";
 import { get_from_storage, set_in_storage } from "./storage";
 import { release_notes_url } from "./version";
 
@@ -7,7 +8,8 @@ const SHOW_RELEASE_NOTES = true;
 const SHOW_RELEASE_NOTES_STORAGE_KEY = "show_release_notes";
 
 export async function maybe_show_release_notes(): Promise<boolean> {
-    if (await get_from_storage<boolean>(SHOW_RELEASE_NOTES_STORAGE_KEY)) {
+    const { show_release_notes } = await options();
+    if (show_release_notes && (await get_from_storage<boolean>(SHOW_RELEASE_NOTES_STORAGE_KEY))) {
         await set_in_storage(SHOW_RELEASE_NOTES_STORAGE_KEY, false);
         await browser.tabs.create({ url: release_notes_url() });
         return true;
